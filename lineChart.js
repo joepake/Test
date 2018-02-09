a4da05b46247c3be9ce37075627365258e2ace6f
Joepake <ohmygodntt@gmail.com>
2018-02-09 16:09:12 +0700

    Create lineChart.js

---
+++ lineChart.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import { VictoryChart, Line, VictoryAxis, VictoryArea, VictoryScatter, VictoryTheme } from 'victory-native';
+
+const data = [
+    { day: '19/09', view: 6 },
+    { day: '20/09', view: 3 },
+    { day: '21/09', view: 5 },
+    { day: '22/09', view: 4 },
+    { day: '23/09', view: 8 },
+    { day: '24/09', view: 2 },
+    { day: '25/09', view: 11 }
+]
+
+export default class LineChartTest extends Component {
+
+    render() {
+        const axisOption = {
+            axis: { stroke: "#7f7f7f", strokeWidth: 0.1 },
+            ticks: { size: 0 }
+        }
+        const gridOption = { strokeWidth: 0.1, fillOpacity: 0.15, color: '#7f7f7f' }
+
+        return (
+            <VictoryChart
+                theme={VictoryTheme.material}
+                height={210}
+                padding={{ top: 10, bottom: 50, left: 35, right: 40 }}>
+                <VictoryScatter
+                    name='aaa'
+                    key='1aaa'
+                    data={data}
+                    x={'day'}
+                    y={'view'}
+                    samples={data.length}
+                    size={3}
+                    style={{
+                        data: { fill: "#7f7f7f" },
+                        labels: {
+                            fontSize: 17,
+                            padding: 10,
+                            fontWeight: 'bold',
+                            fill: '#e46600'
+                        }
+                    }}
+                    events={[
+                        {
+                            childName: "aaa",
+                            target: 'data',
+                            eventHandlers: {
+                                onClick: () => {
+                                    alert("onClick");
+                                },
+                                onPress: () => {
+                                    alert("onPress");
+                                },
+                                onPressIn: () => {
+                                    alert("onPressIn");
+                                },
+                            }
+                        }
+                    ]}
+                />
+                <VictoryAxis
+                    gridComponent={<Line style={gridOption} />}
+                    style={axisOption}
+                    tickFormat={(t) => {
+                        return t
+                    }}
+                />
+                <VictoryAxis
+                    style={axisOption}
+                    dependentAxis
+                    gridComponent={<Line style={gridOption} />}
+                    tickFormat={(t) => `${Math.round(t)}`}
+                    tickCount={4}
+                />
+                <VictoryArea
+                    style={{
+                        data: {
+                            fill: "#074e8e", fillOpacity: 0.15, stroke: "#7f7f7f", strokeWidth: 2
+                        },
+                    }}
+                    data={data}
+                    x={'day'}
+                    y={'view'}
+                />
+            </VictoryChart >
+        )
+    }
+}
