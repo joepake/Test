e75bfee093d5cfe177072beab8ddae68b5e3161c
Joepake <ohmygodntt@gmail.com>
2018-03-15 09:33:26 +0700

    Update lineChart.js

--- lineChart.js
+++ lineChart.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { VictoryChart, Line, VictoryAxis, VictoryArea, VictoryScatter, VictoryTheme } from 'victory-native';
+import Svg from 'react-native-svg';
+
 
 const data = [
     { day: '19/09', view: 6 },
@@ -21,70 +23,67 @@ export default class LineChartTest extends Component {
         const gridOption = { strokeWidth: 0.1, fillOpacity: 0.15, color: '#7f7f7f' }
 
         return (
-            <VictoryChart
-                theme={VictoryTheme.material}
+            //just wrap in Svg tag to fix bug: https://github.com/FormidableLabs/victory-native/issues/254
+            <Svg
+                width={Styles.width}
                 height={210}
-                padding={{ top: 10, bottom: 50, left: 35, right: 40 }}>
-                <VictoryScatter
-                    name='aaa'
-                    key='1aaa'
-                    data={data}
-                    x={'day'}
-                    y={'view'}
-                    samples={data.length}
-                    size={3}
-                    style={{
-                        data: { fill: "#7f7f7f" },
-                        labels: {
-                            fontSize: 17,
-                            padding: 10,
-                            fontWeight: 'bold',
-                            fill: '#e46600'
-                        }
-                    }}
-                    events={[
-                        {
-                            childName: "aaa",
-                            target: 'data',
-                            eventHandlers: {
-                                onClick: () => {
-                                    alert("onClick");
-                                },
-                                onPress: () => {
-                                    alert("onPress");
-                                },
-                                onPressIn: () => {
-                                    alert("onPressIn");
-                                },
+                style={{ width: "100%", height: "auto" }}>
+                <VictoryChart
+                    theme={VictoryTheme.material}
+                    height={210}
+                    padding={{ top: 10, bottom: 50, left: 35, right: 40 }}>
+                    <VictoryScatter
+                        data={data}
+                        x={'day'}
+                        y={'view'}
+                        samples={data.length}
+                        size={3}
+                        style={{
+                            data: { fill: "#7f7f7f" },
+                            labels: {
+                                fontSize: 17,
+                                padding: 10,
+                                fontWeight: 'bold',
+                                fill: '#e46600'
                             }
-                        }
-                    ]}
-                />
-                <VictoryAxis
-                    gridComponent={<Line style={gridOption} />}
-                    style={axisOption}
-                    tickFormat={(t) => {
-                        return t
-                    }}
-                />
-                <VictoryAxis
-                    style={axisOption}
-                    dependentAxis
-                    gridComponent={<Line style={gridOption} />}
-                    tickFormat={(t) => `${Math.round(t)}`}
-                    tickCount={4}
-                />
-                <VictoryArea
-                    style={{
-                        data: {
-                            fill: "#074e8e", fillOpacity: 0.15, stroke: "#7f7f7f", strokeWidth: 2
-                        },
-                    }}
-                    data={data}
-                    x={'day'}
-                    y={'view'}
-                />
-            </VictoryChart >
+                        }}
+                        events={[
+                            {
+                                target: 'data',
+                                eventHandlers: {
+                                    onPress: () => {
+                                        alert("onPress");
+                                    },
+                                }
+                            }
+                        ]}
+                    />
+                    <VictoryAxis
+                        gridComponent={<Line style={gridOption} />}
+                        style={axisOption}
+                        tickFormat={(t) => {
+                            return t
+                        }}
+                    />
+                    <VictoryAxis
+                        style={axisOption}
+                        dependentAxis
+                        gridComponent={<Line style={gridOption} />}
+                        tickFormat={(t) => `${Math.round(t)}`}
+                        tickCount={4}
+                    />
+                    <VictoryArea
+                        style={{
+                            data: {
+                                fill: "#074e8e", fillOpacity: 0.15, stroke: "#7f7f7f", strokeWidth: 2
+                            },
+                        }}
+                        data={data}
+                        x={'day'}
+                        y={'view'}
+                    />
+                </VictoryChart >
+            </Svg>
         )
     }
 }
